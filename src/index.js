0177eb327f09338da755ed9b7ca3fceecc35f659
Anmol <anmolpokhrel46@gmail.com>
2021-06-10 13:06:32 +0530

    dynamic greeting using react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import './style.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// GREETING CODE STARTS 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let currentTime = new Date();
+currentTime = currentTime.getHours();
+let greetingMsg;
+let cssStyle = {};
+if (currentTime <= 11) {
+     greetingMsg = " Good Morning";
+     cssStyle.color = "green";
+} else if (currentTime > 11 && currentTime < 19) {
+     greetingMsg = " Good After-Noon"
+     cssStyle.color = "orange"
+
+} else {
+     greetingMsg = " Good Night";
+     cssStyle.color = " voilet"
+}
+
+
+ReactDom.render(
+
+     <>
+<div className = " main-div">         <h2 className="greeting"> Hello Sir , <span style={cssStyle}> {grettingMsg} </span> </h2>
+</div> 
+
+     </>,
+     document.getElementById('root')
+)
+
+// GREETING CODE ENDS 
+
+
